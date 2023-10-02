d33f53c503f23243f4056796c4109922848017cc
LunarSalameh <lun20190983@std.psut.edu.jo>
2023-10-02 14:51:17 +0300

    Completed the Assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+
+function writeCards(nameArray, msg) {
+    let msgArray = [...nameArray];
+
+    for (let i = 0; i < nameArray.length; i++) {
+        msgArray[i] = `Thank you, ${nameArray[i]}, for the wonderful ${msg} gift!`;
+        console.log(msgArray[i]);
+    }
+
+    return msgArray;
+}
+
+function countDown(value) {
+
+    while (value != -1) {
+        console.log(value);
+        value--;
+    }
+
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+countDown(10);
+countDown(4);
+
